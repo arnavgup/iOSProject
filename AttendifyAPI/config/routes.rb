c289786303fa5f5b4f962ec5115bc7101f145120
George Yao <gyao@andrew.cmu.edu>
2018-10-22 23:23:15 -0400

    fixed for real

--- AttendifyAPI/config/routes.rb
+++ AttendifyAPI/config/routes.rb
@@ -2,15 +2,9 @@ Rails.application.routes.draw do
 
 
     resources :students
-<<<<<<< HEAD
     resources :professors
     resources :courses
     resources :enrollments
-
-    match '/test' => 'professors#new', via: [:get]
-=======
     resources :photos
-    
->>>>>>> caabc45fb94e53672f2984a9be75267de6e55deb
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
